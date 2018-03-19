a32a2769d6471a33120630245a356775205e0ab4
Phil Wareham <phil@designhive.com>
2018-03-19 15:42:37 +0000

    Yep, fix the build

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,18 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
-            Popper: ['popper.js', 'default']
+            Popper: ['popper.js', 'default'],
+            Util: 'exports-loader?Util!bootstrap/js/dist/util',
+            Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+            Button: 'exports-loader?Button!bootstrap/js/dist/button',
+            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+            Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip'
         }),
         new CopyWebpackPlugin([
             {
